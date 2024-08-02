92b548a40ca534662dab87010d6d0a90383fc4e1
Tegar <lanang_islam_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:53:47 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,6 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Tegar dari kelas X RPL 7 yang berasal dari kabupaten Kediri");
+        String a = "1";     
+    }
+}
